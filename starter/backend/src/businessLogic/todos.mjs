c7a703a06e41505e9bf5e4588b51e7c01496a4fb
kid8888 <sunyi7331@gmail.com>
2024-12-26 13:28:34 +0900

    重命名 todos 相关函数以提高可读性，新增测试事件 JSON 文件

--- starter/backend/src/businessLogic/todos.mjs
+++ starter/backend/src/businessLogic/todos.mjs
@@ -5,7 +5,7 @@ import * as uuid from 'uuid'
 const log = createLogger('TodosService')
 const todosAccess = new TodosAccess()
 
-export const create = async (todoData, userId) => {
+export const createTodo = async (todoData, userId) => {
   if (!userId) {
     log.info('User not authenticated')
     return false
@@ -39,7 +39,7 @@ export const getAll = async (userId) => {
   return await todosAccess.getAll(userId)
 }
 
-export const update = async (userId, todoId, updatedData) => {
+export const updateTodo = async (userId, todoId, updatedData) => {
   if (!userId) {
     log.info('User not authenticated')
     return false
@@ -48,7 +48,7 @@ export const update = async (userId, todoId, updatedData) => {
   return true
 }
 
-export const remove = async (userId, todoId) => {
+export const deleteTodo = async (userId, todoId) => {
   if (!userId) {
     log.info('User not authenticated')
     return false
