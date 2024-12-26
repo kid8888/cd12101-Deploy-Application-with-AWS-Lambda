09f54fa800465afb7ffbbe36865e2361156086e6
kid8888 <sunyi7331@gmail.com>
2024-12-26 22:05:45 +0900

    更新 API Gateway 配置，添加 Auth0 管理 API 标识符

--- starter/client/src/config.ts
+++ starter/client/src/config.ts
@@ -1,10 +1,10 @@
-const apiId = 'b1yikvwoq5'; // Your API Gateway ID
-const region = 'us-east-1'; // Your AWS region
+const apiId = 'b1yikvwoq5' // 替换为你的 API Gateway ID
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev/todos`
 
-export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`;
 
 export const authConfig = {
-  domain: 'dev-2xo0ymz2yc3zekoc.us.auth0.com', // Your Auth0 domain
-  clientId: 'your-client-id',              // Your Auth0 client ID
-  callbackUrl: 'http://localhost:3000/callback' // Callback URL for local dev
-};
+  domain: 'dev-2xo0ymz2yc3zekoc.us.auth0.com', // Auth0 域名
+  clientId: 'UQz2SltzRYftmz8KA69xPYRqyV9N8MAZ', // Auth0 应用的客户端 ID
+  audience: 'https://dev-2xo0ymz2yc3zekoc.us.auth0.com/api/v2/', // **Auth0 Management API 标识符**
+  callbackUrl: 'http://localhost:3000/callback', // 回调 URL
+}
