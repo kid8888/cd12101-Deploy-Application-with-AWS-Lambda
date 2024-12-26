c263e733a434cedf189cd47e4ddb752dfee1de1e
kid8888 <sunyi7331@gmail.com>
2024-12-26 10:24:59 +0900

    add codes

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,30 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getUserId } from '../utils.mjs'
+import { deleteTodo } from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+    await deleteTodo(userId, todoId)
 
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        message: "todo deleted successfully"
+      })
+    }
+  }
+)
--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,41 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getUserId } from '../utils.mjs'
+import { getTodo, updateTodo } from '../../businessLogic/todos.mjs'
+import { getUploadUrl, getPublicUrl } from '../../fileStorage/attachmentUtils.mjs'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
+    const todo = await getTodo(userId, todoId)
 
+    if(!todo) {
+      throw new Error(`Todo ${todoId} does not exist`)
+    }
+
+    // generate upload url
+    const uploadUrl = await getUploadUrl(todoId)
+    const publicUrl = getPublicUrl(todoId)
+
+    // update todo with attachment url
+    await updateTodo(userId, todoId, {
+      attachmentUrl: publicUrl
+    })
+
+    // return a presigned URL to upload a file for a TODO item with the provided id
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  }
+)
